dca1268fdbfc4e40dc117524ad07c56b2c3c6423
Alex King <calicoalix@gmail.com>
2023-08-01 23:38:32 +1000

    Initial Commit

---
+++ yuzu/yuzuApp.swift
@@ -0,0 +1,17 @@
+//
+//  yuzuApp.swift
+//  yuzu
+//
+//  Created by Alex King on R 5/08/01.
+//
+
+import SwiftUI
+
+@main
+struct yuzuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
