059789de574eba51002657127bba2f64df402017
Alex King <calicoalix@gmail.com>
2023-08-02 22:47:10 +1000

    configured custom svg icons in taskbar - handled dark and light mode conditional rendering

--- yuzu/Views/TabBar/TabBarView.swift
+++ yuzu/Views/TabBar/TabBarView.swift
@@ -8,32 +8,65 @@
 import SwiftUI
 
 struct TabBarView: View {
+    @State private var selectedTab = 0
+    @Environment (\.colorScheme) var colorScheme
+    
     var body: some View {
-        TabView {
+        TabView(selection: $selectedTab) {
             HomeView()
                 .tabItem {
-                    Label("Home", systemImage: "house.fill")
+                    Image(
+                        selectedTab == 0 ?
+                        (colorScheme == .dark ? "House-Solid-Dark" : "House-Solid")
+                        :
+                        (colorScheme == .dark ? "House-Unselected-Dark" : "House-Unselected")
+                    )
                 }
+                .tag(0)
 
             SearchView()
                 .tabItem {
-                    Label("Search", systemImage: "magnifyingglass")
+                    Image(
+                        selectedTab == 1 ?
+                        (colorScheme == .dark ? "Search-Selected-Dark" : "Search-Selected-Light")
+                        :
+                        (colorScheme == .dark ? "Search-Unselected-Dark" : "Search-Unselected-Light")
+                    )
                 }
+                .tag(1)
 
             PostCreateView()
                 .tabItem {
-                    Label("Post", systemImage: "plus.square.fill")
+                    Image(
+                        selectedTab == 2 ?
+                        (colorScheme == .dark ? "Post-Create-Selected-Dark" : "Post-Create-Selected-Light")
+                        :
+                        (colorScheme == .dark ? "Post-Create-Unselected-Dark" : "Post-Create-Unselected-Light")
+                        )
                 }
+                .tag(2)
 
             NotificationsView()
                 .tabItem {
-                    Label("Notifications", systemImage: "bell.fill")
+                    Image(
+                        selectedTab == 3 ?
+                        (colorScheme == .dark ? "Notifications-Selected-Dark" : "Notifications-Selected-Light")
+                        :
+                        (colorScheme == .dark ? "Notifications-Unselected-Dark" : "Notifications-Unselected-Light")
+                        )
                 }
-
+                .tag(3)
+            
             ProfileView()
                 .tabItem {
-                    Label("Profile", systemImage: "person.fill")
+                    Image(
+                        selectedTab == 4 ?
+                        (colorScheme == .dark ? "Profile-Selected-Dark" : "Profile-Selected-Light")
+                        :
+                        (colorScheme == .dark ? "Profile-Unselected-Dark" : "Profile-Unselected-Light")
+                    )
                 }
+                .tag(4)
         }
     }
 }
