85348003d56d4c332907dda75f92d46798aee68a
Alex King <calicoalix@gmail.com>
2023-08-09 13:30:18 +1000

    tweaked header hide and appear logic as well as reenabled pull down to refresh on homefeedview

--- yuzu/Views/Home/HomeFeedView.swift
+++ yuzu/Views/Home/HomeFeedView.swift
@@ -8,37 +8,63 @@
 import SwiftUI
 
 struct HomeFeedView: View {
-    @State private var isRefreshing = false
     @State private var items = [Int](1...10)
+    @Binding var isRefreshing: Bool
+    @State private var pullOffset: CGFloat = 0
     
     var body: some View {
-        VStack{
-            List {
-                ForEach(items, id: \.self) { item in
-                    Text("Post \(item)")
-                        .padding()
-                }
+        VStack {
+            if isRefreshing {
+                ProgressView()
+                    .progressViewStyle(CircularProgressViewStyle())
+                    .padding()
             }
-            .frame(height:800)
-            .refreshable {
-                isRefreshing = true
-                
-                DispatchQueue.global().asyncAfter(deadline: .now() + 1) {
-                    items.shuffle()
-                    
-                    DispatchQueue.main.async {
-                        isRefreshing = false
+            
+            ScrollView {
+                VStack {
+                    ForEach(items, id: \.self) { item in
+                        Text("Post \(item)")
+                            .padding()
+                            .frame(height:200)
                     }
+                    
                 }
-                
+                .offset(y: pullOffset)
+                .gesture(DragGesture(coordinateSpace: .local)
+                            .onChanged { value in
+                                if value.translation.height > 0 {
+                                    pullOffset = value.translation.height
+                                }
+                            }
+                            .onEnded { value in
+                                if pullOffset > 50 {
+                                    refresh()
+                                }
+                                withAnimation {
+                                    pullOffset = 0
+                                }
+                            }
+                )
             }
         }
+    }
+    
+    private func refresh() {
+        isRefreshing = true
         
+        DispatchQueue.global().asyncAfter(deadline: .now() + 1) {
+            items.shuffle()
+            
+            DispatchQueue.main.async {
+                isRefreshing = false
+            }
+        }
     }
 }
 
-struct HomeFeedView_Previews: PreviewProvider {
-    static var previews: some View {
-        HomeFeedView()
-    }
-}
+//
+//struct HomeFeedView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        HomeFeedView(isRefreshing:false)
+//    }
+//}
--- yuzu/Views/Home/HomeView.swift
+++ yuzu/Views/Home/HomeView.swift
@@ -11,6 +11,8 @@ import SwiftUI
 struct HomeView: View {
     
     @StateObject var headerData = HomeHeaderViewModel()
+    @State private var isScrolling: Bool = false
+    @State private var isRefreshing = false
     
     init() {
         UIScrollView.appearance().bounces = false
@@ -23,64 +25,79 @@ struct HomeView: View {
             HomeHeaderView()
                 .zIndex(1)
                 .offset(y: headerData.headerOffset)
-            ScrollView(.vertical, showsIndicators: false, content: {
-                HomeFeedView()
-                    .overlay(
-                        GeometryReader{proxy -> Color in
-                            let minY = proxy.frame(in: .global).minY
-                            
-                            DispatchQueue.main.async {
-                                if headerData.startMinY == 0 {
-                                    headerData.startMinY = minY
-                                }
+            ZStack {
+                CustomScrollViewRepresentable(isScrolling: $isScrolling) {
+                    HomeFeedView(isRefreshing: $isRefreshing)
+                        .overlay(
+                            GeometryReader{proxy -> Color in
+                                let minY = proxy.frame(in: .global).minY
                                 
-                                let offset = headerData.startMinY - minY
-                                
-
-                                if offset > headerData.offset {
-
-                                    headerData.bottomScrollOffset = 0
-                                    
-                                    if headerData.topScrollOffset == 0 {
-                                        headerData.topScrollOffset = offset
+                                DispatchQueue.main.async {
+                                    if headerData.startMinY == 0 {
+                                        headerData.startMinY = minY
                                     }
                                     
-                                    let progress = (headerData.topScrollOffset + getMaxOffset()) - offset
-                                    
-                                    let offsetCondition = (headerData.topScrollOffset + getMaxOffset()) >= getMaxOffset() && getMaxOffset() - progress <= getMaxOffset()
-                                    
-                                    let headerOffset = offsetCondition ? -(getMaxOffset() - progress) : -getMaxOffset()
+                                    let offset = headerData.startMinY - minY
 
-                                    headerData.headerOffset = headerOffset
-                                    
-                                }
-                                if offset < headerData.offset {
-                                    headerData.topScrollOffset = 0
-                                    if headerData.bottomScrollOffset == 0 {
-                                        headerData.bottomScrollOffset = offset
-                                    }
-                                    
-                                    withAnimation(.easeOut(duration: 0.25)) {
-//                                        let headerOffset = headerData.headerOffset
-                                        if headerData.bottomScrollOffset > offset + 40 {
-                                            headerData.headerOffset = 0
-                                            
+                                    if offset > headerData.offset {
+
+                                        headerData.bottomScrollOffset = 0
+                                        
+                                        if headerData.topScrollOffset == 0 {
+                                            headerData.topScrollOffset = offset
                                         }
-//                                        headerData.headerOffset = headerData.bottomScrollOffset > offset + 40 ? 0 : (headerOffset != -getMaxOffset() ? 0 : headerOffset)
+                                        
+                                        let progress = (headerData.topScrollOffset + getMaxOffset()) - offset
+                                        
+                                        let offsetCondition = (headerData.topScrollOffset + getMaxOffset()) >= getMaxOffset() && getMaxOffset() - progress <= getMaxOffset()
+                                        
+                                        let headerOffset = offsetCondition ? -(getMaxOffset() - progress) : -getMaxOffset()
+
+                                        headerData.headerOffset = headerOffset
                                         
                                     }
+                                    if offset < headerData.offset {
+                                        
+                                        headerData.topScrollOffset = 0
+                                        if headerData.bottomScrollOffset == 0 {
+                                            headerData.bottomScrollOffset = offset
+                                        }
+                                        
+                                        withAnimation(.easeOut(duration: 0.25)) {
+                                            let headerOffset = headerData.headerOffset
+                                            if headerData.bottomScrollOffset > offset {
+                                                headerData.headerOffset = 0
+                                            } else {
+                                                headerData.headerOffset = headerOffset
+                                            }
+                                            
+                                            
+    //                                        headerData.headerOffset = headerData.bottomScrollOffset > offset + 40 ? 0 : (headerOffset != -getMaxOffset() ? 0 : headerOffset)
+                                            
+                                        }
+                                    }
+                                    
+                                    headerData.offset = offset
                                 }
                                 
-                                headerData.offset = offset
+                                
+                                return Color.clear
                             }
-                            
-                            
-                            return Color.clear
+                                .frame(height: 1)
+                                ,alignment:.top
+                        )
+                        
+                }
+                .onChange(of: isScrolling) { newValue in
+                    if !newValue && headerData.headerOffset < 0 {
+                        withAnimation(.easeOut(duration: 0.25)) {
+                            headerData.headerOffset = -177
                         }
-                            .frame(height: 1)
-                            ,alignment:.top
-                    )
-            })
+                    }
+                }
+            }
+            
+            
         }
     }
     
@@ -98,3 +115,70 @@ struct HomeView_Preview: PreviewProvider {
 }
 
 var edges = UIApplication.shared.windows.first?.safeAreaInsets
+
+
+struct CustomScrollViewRepresentable<Content: View>: UIViewRepresentable {
+    @Binding var isScrolling: Bool
+    let content: Content
+
+    init(isScrolling: Binding<Bool>, @ViewBuilder content: () -> Content) {
+        self._isScrolling = isScrolling
+        self.content = content()
+    }
+
+    func makeCoordinator() -> CustomScrollViewDelegate {
+        CustomScrollViewDelegate()
+    }
+
+    func makeUIView(context: Context) -> UIScrollView {
+        let scrollView = UIScrollView()
+        scrollView.delegate = context.coordinator
+        context.coordinator.onScrollStart = {
+            self.isScrolling = true
+        }
+        context.coordinator.onScrollEnd = {
+            self.isScrolling = false
+        }
+        let hostingController = UIHostingController(rootView: content)
+        hostingController.view.translatesAutoresizingMaskIntoConstraints = false
+        scrollView.addSubview(hostingController.view)
+        NSLayoutConstraint.activate([
+            hostingController.view.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
+            hostingController.view.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor),
+            hostingController.view.topAnchor.constraint(equalTo: scrollView.topAnchor),
+            hostingController.view.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
+            hostingController.view.widthAnchor.constraint(equalTo: scrollView.widthAnchor)
+        ])
+        return scrollView
+    }
+
+    func updateUIView(_ uiView: UIScrollView, context: Context) {
+        context.coordinator.onScrollStart = {
+            self.isScrolling = true
+            
+        }
+        context.coordinator.onScrollEnd = {
+            self.isScrolling = false
+        
+        }
+    }
+}
+
+class CustomScrollViewDelegate: NSObject, UIScrollViewDelegate {
+    var onScrollStart: (() -> Void)?
+    var onScrollEnd: (() -> Void)?
+
+    func scrollViewWillBeginDragging(_ scrollView: UIScrollView) {
+        onScrollStart?()
+    }
+    
+    func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
+        if !decelerate {
+            onScrollEnd?()
+        }
+    }
+    
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        onScrollEnd?()
+    }
+}
