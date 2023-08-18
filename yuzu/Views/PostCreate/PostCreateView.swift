1e54bb7b8ee322504320c690e7625c3a701f58ce
Alex King <calicoalix@gmail.com>
2023-08-18 23:51:16 +1000

    refactored swipe actions in tabbarview

--- yuzu/Views/PostCreate/PostCreateView.swift
+++ yuzu/Views/PostCreate/PostCreateView.swift
@@ -6,83 +6,126 @@
 //
 
 import SwiftUI
-import Combine
 
-struct PostCreateView: View {
+struct TopBarView: View {
+    
     @Environment(\.presentationMode) var presentationMode
-    @State private var postText: String = ""
-    @State private var image: Image? = nil
-    @State private var isImagePickerShown: Bool = false
-    @Binding var isPostCreateSheetVisible: Bool
+    @Binding var postText: String
+    @Binding var postStatus: PostStatus?
     var isPostButtonDisabled: Bool {
         postText.trimmingCharacters(in: .whitespacesAndNewlines).isEmpty
     }
+    
+    var body: some View {
+        HStack {
+            Button(action: {
+                presentationMode.wrappedValue.dismiss()
+            }) {
+                Text("Cancel")
+            }
+            .padding(.leading)
+            .foregroundColor(.primary)
+
+            Spacer()
+            
+            Button(action: {
+                presentationMode.wrappedValue.dismiss()
+                postStatus = .submitting(progress: 0.0)
+                
+                Timer.scheduledTimer(withTimeInterval: 0.2, repeats: true) { timer in
+                    DispatchQueue.main.async { // Ensure we're on the main thread
+                        if case .submitting(let progress) = self.postStatus, progress < 1.0 {
+                            self.postStatus = .submitting(progress: progress + 0.2)
+                        } else {
+                            timer.invalidate()
+                            
+                            if self.postText.contains("error") {
+                                self.postStatus = .failed
+                            } else {
+                                self.postStatus = .success
+                            }
+                        }
+                    }
+                }
+
+            }) {
+                Text("Post")
+            }
+            .padding(.trailing)
+            .foregroundColor(isPostButtonDisabled ? .gray : .primary)
+            .disabled(isPostButtonDisabled)
+        }
+        .overlay(
+            Text("New Post")
+                .font(.headline)
+        )
+    }
+}
+
+struct PostInputView: View {
+    
+    @Binding var isImagePickerShown: Bool
+//    @Binding var isPostCreateSheetVisible: Bool
+    @Binding var postText: String
+    
+    var body: some View {
+        HStack(alignment:.top) {
+            Circle()
+                .fill(Color.gray)
+                .frame(width: 40, height: 40)
+            VStack(alignment: .leading) {
+                TextField("What's on your mind?", text: $postText, axis: .vertical)
+                    .lineLimit(nil)
+                    .padding()
+                    .background(Color.gray.opacity(0.1))
+                    .cornerRadius(10)
+                    .keyboardType(.default)
+                        .autocapitalization(.none)
+                        .disableAutocorrection(true)
+                        .textContentType(.none)
+                        .submitLabel(.return)
+                        .onSubmit {
+                            
+                            print("Post button pressed!")
+//                            isPostCreateSheetVisible.toggle()
+                        }
+                    
+                    Button(action: {
+                        isImagePickerShown.toggle()
+                    }) {
+                        Image(systemName: "paperclip")
+                            .resizable()
+                            .frame(width: 20, height: 20)
+                    }
+                    .padding(.top, 5)
+                }
+                
+                Spacer()
+
+        }
+        .padding(.vertical)
+        .padding(.horizontal)
+    }
+}
+
+struct PostCreateView: View {
+    
+
+    @Environment(\.presentationMode) var presentationMode
+    
+    @State private var postText: String = ""
+    @State private var image: Image? = nil
+    @State private var isImagePickerShown: Bool = false
+//    @Binding var isPostCreateSheetVisible: Bool
+    @Binding var isSubmittingPost: Bool
+    @Binding var postStatus: PostStatus?
+    
+    
 
     var body: some View {
         VStack {
-            HStack {
-                Button(action: {
-                    presentationMode.wrappedValue.dismiss()
-                }) {
-                    Text("Cancel")
-                }
-                .padding(.leading)
-                .foregroundColor(.primary)
-
-                Spacer()
-                
-                Button(action: {
-                    presentationMode.wrappedValue.dismiss()
-                }) {
-                    Text("Post")
-                }
-                .padding(.trailing)
-                .foregroundColor(isPostButtonDisabled ? .gray : .primary)
-                .disabled(isPostButtonDisabled)
-            }
-            .overlay(
-                Text("New Post")
-                    .font(.headline)
-            )
-            
-            HStack(alignment:.top) {
-                Circle()
-                    .fill(Color.gray)
-                    .frame(width: 40, height: 40)
-                VStack(alignment: .leading) {
-                    TextField("What's on your mind?", text: $postText, axis: .vertical)
-                        .lineLimit(nil)
-                        .padding()
-                        .background(Color.gray.opacity(0.1))
-                        .cornerRadius(10)
-                        .keyboardType(.default)
-                            .autocapitalization(.none)
-                            .disableAutocorrection(true)
-                            .textContentType(.none)
-                            .submitLabel(.return)
-                            .onSubmit {
-                                
-                                print("Post button pressed!")
-                                isPostCreateSheetVisible.toggle()
-                            }
-                        
-                        Button(action: {
-                            isImagePickerShown.toggle()
-                        }) {
-                            Image(systemName: "paperclip")
-                                .resizable()
-                                .frame(width: 20, height: 20)
-                        }
-                        .padding(.top, 5)
-                    }
-                    
-                    Spacer()
-
-            }
-            .padding(.vertical)
-            .padding(.horizontal)
-            
-
+            TopBarView(postText: $postText, postStatus: $postStatus)
+            PostInputView(isImagePickerShown: $isImagePickerShown, postText: $postText)
 
             Spacer()
 
@@ -94,9 +137,9 @@ struct PostCreateView: View {
 
         }
         .padding(.top, 20)
-        .sheet(isPresented: $isImagePickerShown, onDismiss: loadImage) {
-//            ImagePicker(image: $image)
-        }
+//        .sheet(isPresented: $isImagePickerShown, onDismiss: loadImage) {
+////            ImagePicker(image: $image)
+//        }
     }
 
     func loadImage() {
--- yuzu/Views/TabBar/TabBarView.swift
+++ yuzu/Views/TabBar/TabBarView.swift
@@ -17,6 +17,11 @@ extension Binding {
         })
     }
 }
+enum PostStatus {
+    case submitting(progress: Double)
+    case success
+    case failed
+}
 
 
 struct TabBarView: View {
@@ -30,158 +35,162 @@ struct TabBarView: View {
     @State private var highlightedTab: Int?
     @State private var isSidebarVisible: Bool = false
     @State private var xOffset: CGFloat = 0.0
-    
     @ObservedObject var model = TabModel()
-    
     @State private var isPostDetailViewPresented: Bool = false
-    
     @State private var sideBarSize:CGFloat = 275
+    @State private var isSubmittingPost: Bool = false
+    @State private var postStatus: PostStatus? = nil
+
+
     
     var overlayOpacity: Double {
         Double(xOffset / self.sideBarSize) * 0.5
     }
-    
+    func sidebarGesture() -> some Gesture {
+        DragGesture()
+            .onChanged { value in
+                if abs(value.translation.width) > abs(value.translation.height) {
+                    if value.translation.width > 0 {
+                        self.xOffset = min(self.sideBarSize, value.translation.width)
+                    } else if self.isSidebarVisible {
+                        self.xOffset = min(self.sideBarSize, max(0, self.sideBarSize + value.translation.width))
+                    }
+                }
+            }
+            .onEnded { value in
+                if abs(value.translation.width) > abs(value.translation.height) {
+                    withAnimation(.easeInOut(duration: 0.25)) {
+                        if self.xOffset < 200 && isSidebarVisible {
+                            self.isSidebarVisible = false
+                            self.xOffset = 0
+                        } else if self.xOffset > 50{
+                            self.isSidebarVisible = true
+                            self.xOffset = self.sideBarSize
+                        } else {
+                            self.isSidebarVisible = false
+                            self.xOffset = 0
+                        }
+                    }
+                }
+            }
+    }
     
     var body: some View {
-        ZStack(alignment: .leading) {
-            SidebarMenuView()
-                .frame(width: self.sideBarSize)
+        GeometryReader { geometry in
+            ZStack(alignment: .leading) {
+                SidebarMenuView()
+                    .frame(width: self.sideBarSize)
                 
-            
-            TabView(selection: $selectedTab.onUpdate{ model.selectTab(item: selectedTab, lastSelectedTab: latestSelectedTab) }) {
-                HomeView(isPostDetailViewPresented: $isPostDetailViewPresented, model: model)
-                   
+                
+                TabView(selection: $selectedTab.onUpdate{ model.selectTab(item: selectedTab, lastSelectedTab: latestSelectedTab) }) {
+                    HomeView(isPostDetailViewPresented: $isPostDetailViewPresented, model: model)
                     
-                    .tabItem {
-                        Image(
-                            selectedTab == 0 || highlightedTab == 0 ?
-                            (colorScheme == .dark ? "House-Solid-Dark" : "House-Solid")
-                            :
-                                (colorScheme == .dark ? "House-Unselected-Dark" : "House-Unselected")
-                        )
-                        .padding(.top, 100)
-                        
-                    }
-                    .tag(0)
-                
-                SearchView()
-                    .tabItem {
-                        Image(
-                            selectedTab == 1 ?
-                            (colorScheme == .dark ? "Search-Selected-Dark" : "Search-Selected-Light")
-                            :
-                                (colorScheme == .dark ? "Search-Unselected-Dark" : "Search-Unselected-Light")
-                        )
-                    }
                     
-                    .tag(1)
-                
-                TempBlankView(isPostCreateSheetVisible: $isPostCreateSheetVisible,
-                              selectedTab: $selectedTab,
-                              previousSelectedTab: $previousSelectedTab)
-                .tabItem {
-                    Image(
-                        selectedTab == 2 ?
-                        (colorScheme == .dark ? "Post-Create-Selected-Dark" : "Post-Create-Selected-Light")
-                        :
-                            (colorScheme == .dark ? "Post-Create-Unselected-Dark" : "Post-Create-Unselected-Light")
-                    )
-                }
-                .tag(2)
-                
-                NotificationsView()
+                        .tabItem {
+                            Image(
+                                selectedTab == 0 || highlightedTab == 0 ?
+                                (colorScheme == .dark ? "House-Solid-Dark" : "House-Solid")
+                                :
+                                    (colorScheme == .dark ? "House-Unselected-Dark" : "House-Unselected")
+                            )
+                            .padding(.top, 100)
+                            
+                        }
+                        .tag(0)
+                    
+                    SearchView()
+                        .tabItem {
+                            Image(
+                                selectedTab == 1 ?
+                                (colorScheme == .dark ? "Search-Selected-Dark" : "Search-Selected-Light")
+                                :
+                                    (colorScheme == .dark ? "Search-Unselected-Dark" : "Search-Unselected-Light")
+                            )
+                        }
+                    
+                        .tag(1)
+                    
+                    TempBlankView(isPostCreateSheetVisible: $isPostCreateSheetVisible,
+                                  selectedTab: $selectedTab,
+                                  previousSelectedTab: $previousSelectedTab)
                     .tabItem {
                         Image(
-                            selectedTab == 3 ?
-                            (colorScheme == .dark ? "Notifications-Selected-Dark" : "Notifications-Selected-Light")
+                            selectedTab == 2 ?
+                            (colorScheme == .dark ? "Post-Create-Selected-Dark" : "Post-Create-Selected-Light")
                             :
-                                (colorScheme == .dark ? "Notifications-Unselected-Dark" : "Notifications-Unselected-Light")
+                                (colorScheme == .dark ? "Post-Create-Unselected-Dark" : "Post-Create-Unselected-Light")
                         )
                     }
-                    .tag(3)
-                
-                ProfileView()
-                    .tabItem {
-                        Image(
-                            selectedTab == 4 ?
-                            (colorScheme == .dark ? "Profile-Selected-Dark" : "Profile-Selected-Light")
-                            :
-                                (colorScheme == .dark ? "Profile-Unselected-Dark" : "Profile-Unselected-Light")
-                        )
-                        
-                    }
-                    .tag(4)
-                
-            }
-            .overlay(
-                Rectangle()
-                    .fill(Color.black.opacity(overlayOpacity))
-                    .ignoresSafeArea()
-            )
-            .overlay(
-                Group {
-                    if xOffset > 0 {
-                        Rectangle()
-                            .fill(Color.black.opacity(overlayOpacity))
-                            .ignoresSafeArea()
-                            .onTapGesture {
-                                withAnimation {
-                                    isSidebarVisible = false
-                                    self.xOffset = 0
-                                    
-                                }
-                            }
-                    }
+                    .tag(2)
+                    
+                    NotificationsView()
+                        .tabItem {
+                            Image(
+                                selectedTab == 3 ?
+                                (colorScheme == .dark ? "Notifications-Selected-Dark" : "Notifications-Selected-Light")
+                                :
+                                    (colorScheme == .dark ? "Notifications-Unselected-Dark" : "Notifications-Unselected-Light")
+                            )
+                        }
+                        .tag(3)
+                    
+                    ProfileView()
+                        .tabItem {
+                            Image(
+                                selectedTab == 4 ?
+                                (colorScheme == .dark ? "Profile-Selected-Dark" : "Profile-Selected-Light")
+                                :
+                                    (colorScheme == .dark ? "Profile-Unselected-Dark" : "Profile-Unselected-Light")
+                            )
+                            
+                        }
+                        .tag(4)
                 }
-            )
-            .onChange(of: selectedTab) { newTab in
-                
-                previousSelectedTab = latestSelectedTab
-                latestSelectedTab = newTab
-            }
-            .sheet(isPresented: $isPostCreateSheetVisible) {
-                PostCreateView(isPostCreateSheetVisible: $isPostCreateSheetVisible)
-            }
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
-            .offset(x: xOffset)
-            
-            .gesture(
-                selectedTab == 0 && !self.isPostDetailViewPresented ? DragGesture()
-                    .onChanged { value in
-                        // Check if the gesture is more horizontal than vertical
-                        if abs(value.translation.width) > abs(value.translation.height) {
-                            if value.translation.width > 0 {
-                                self.xOffset = min(self.sideBarSize, value.translation.width)
-                            } else if self.isSidebarVisible {
-                                self.xOffset = min(self.sideBarSize, max(0, self.sideBarSize + value.translation.width))
-                            }
+                .overlay(
+                    Group {
+                        if isSubmittingPost, let status = postStatus {
+                            PostStatusOverlayView(postStatus: status)
                         }
                     }
-                    .onEnded { value in
-                        if abs(value.translation.width) > abs(value.translation.height) {
-                            withAnimation(.easeInOut(duration: 0.25)) {
-                                if self.xOffset < 200 && isSidebarVisible {
-                                    self.isSidebarVisible = false
-                                    self.xOffset = 0
-                                } else if self.xOffset > 50{
-                                    self.isSidebarVisible = true
-                                    self.xOffset = self.sideBarSize
-                                } else {
-                                    self.isSidebarVisible = false
-                                    self.xOffset = 0
-                                }
-                            }
-                        }
-                    } :
-                    nil
+                )
+                .overlay(
+                    Rectangle()
+                        .fill(Color.black.opacity(overlayOpacity))
+                        .ignoresSafeArea()
+                )
+                .overlay(
+                    Group {
+                        if xOffset > 0 {
+                            Rectangle()
+                                .fill(Color.black.opacity(overlayOpacity))
+                                .ignoresSafeArea()
+                                .onTapGesture {
+                                    withAnimation {
+                                        isSidebarVisible = false
+                                        self.xOffset = 0
 
-            )
-            
+                                    }
+                                }
+                        }
+                    }
+                )
+                .onChange(of: selectedTab) { newTab in
+                    previousSelectedTab = latestSelectedTab
+                    latestSelectedTab = newTab
+                }
+//                .sheet(isPresented: $isPostCreateSheetVisible) {
+//                    PostCreateView()
+//                }
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .offset(x: xOffset)
+                .gesture(selectedTab == 0 && !self.isPostDetailViewPresented ? sidebarGesture() : nil)
+            }
             
         }
     }
-    
 }
+
+
 class TabModel: ObservableObject {
     var scrollViewCoordinator: CustomScrollViewRepresentable<HomeFeedView>.Coordinator?
 
@@ -193,6 +202,30 @@ class TabModel: ObservableObject {
     }
 }
 
+struct PostStatusOverlayView: View {
+    var postStatus: PostStatus
+
+    var body: some View {
+        VStack {
+            Spacer()
+            HStack {
+                switch postStatus {
+                case .submitting:
+                    Text("Submitting post...")
+                case .success:
+                    Text("Successfully submitted!")
+                case .failed:
+                    Text("Submission failed!")
+                }
+                Spacer()
+                CustomProgressView(postStatus: postStatus)
+            }
+            .padding()
+            .background(Color.gray.opacity(0.2))
+            .cornerRadius(15)
+        }
+    }
+}
 
 
 
@@ -218,3 +251,27 @@ struct TempBlankView: View {
         
     }
 }
+struct CustomProgressView: View {
+    var postStatus: PostStatus
+    var body: some View {
+        switch postStatus {
+        case .submitting(let progress):
+            Circle()
+                .trim(from: 0.0, to: CGFloat(progress))
+                .stroke(Color.blue, lineWidth: 4)
+                .rotationEffect(Angle(degrees: -90))
+                .frame(width: 30, height: 30)
+        case .success:
+            Image(systemName: "checkmark.circle.fill")
+                .foregroundColor(.green)
+                .font(.system(size: 30))
+        case .failed:
+            Image(systemName: "xmark.circle.fill")
+                .foregroundColor(.red)
+                .font(.system(size: 30))
+        }
+    }
+}
+
+
+
